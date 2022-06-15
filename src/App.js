8a089e2cc8ec93134ae0cd77e3e76d24c4d8857b
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-06-16 05:24:55 +0700

    change title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<h1>hello from homepage</h1>
+				<h1>hello from fadhil</h1>
 			</header>
 		</div>
 	);
